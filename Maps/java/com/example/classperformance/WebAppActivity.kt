6501fd77959e74ec16671c039037d1be7737a55d
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2025-10-26 19:27:29 +0530

    Add files via upload

---
+++ Maps/java/com/example/classperformance/WebAppActivity.kt
@@ -0,0 +1,37 @@
+package com.example.classperformance
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageView
+import kotlin.jvm.java
+
+//main
+
+class WebAppActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_app)
+
+        val links = mapOf(
+            R.id.iccLogo to "https://www.icc-cricket.com/",
+            R.id.wwe to "https://wwe.com",
+            R.id.fifaLogo to "https://www.fifa.com/",
+            R.id.tableTennisLogo to "https://www.ittf.com/",
+            R.id.badmintonLogo to "https://bwfbadminton.com/",
+            R.id.formula1 to "https://www.formula1.com/",
+            R.id.lawnTennisLogo to "https://www.itftennis.com/",
+            R.id.basketballLogo to "https://www.fiba.basketball/",
+            R.id.taekwondoLogo to "https://www.worldtaekwondo.org/"
+        )
+
+        for ((id, url) in links) {
+            findViewById<ImageView>(id).setOnClickListener {
+                val intent = Intent(this, WebViewActivity::class.java)
+                intent.putExtra("url", url)
+                startActivity(intent)
+            }
+        }
+    }
+}
